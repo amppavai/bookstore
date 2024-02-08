2c2ffa13996d939c9c1c5b211b32c611502a9a05
amppavai <ani.vainio@myy.haaga-helia.fi>
2024-02-08 12:36:42 +0200

    lisäyksiä: CommandLineRunner unohtui, application.properties päivitetty

--- src/main/java/hh/sof03/bookstore/BookstoreApplication.java
+++ src/main/java/hh/sof03/bookstore/BookstoreApplication.java
@@ -1,7 +1,12 @@
 package hh.sof03.bookstore;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import hh.sof03.bookstore.domain.Book;
+import hh.sof03.bookstore.domain.BookRepository;
 
 @SpringBootApplication
 public class BookstoreApplication {
@@ -10,4 +15,11 @@ public class BookstoreApplication {
 		SpringApplication.run(BookstoreApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner demo(BookRepository bookRepository) {
+		return (args) -> {
+			bookRepository.save(new Book(0, "Salamurhaajan oppipoika", "Robin Hobb", 1996, "978-951-1-32821-61", 18.50));
+			bookRepository.save(new Book(0, "Näkymättömät naiset", "Caroline Criado Perez", 2020, "978-951-0-4466-52", 24.90));
+		};
+	}
 }
--- src/main/java/hh/sof03/bookstore/web/BookstoreController.java
+++ src/main/java/hh/sof03/bookstore/web/BookstoreController.java
@@ -11,34 +11,38 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import hh.sof03.bookstore.domain.Book;
 
-
 @Controller
 public class BookstoreController {
-    // b.) Add a new controller called BookController which handle get request to the path /index 
+    // b.) Add a new controller called BookController which handle get request to
+    // the path /index
     @RequestMapping(value = "/index", method = RequestMethod.GET)
     public String getBooks(Model model) {
         List<Book> books = new ArrayList<>();
-        books.add(new Book(0, "Kirja 1", "Ernest Hemingway", 1929, "vc5y45uv236", 10));
-        books.add(new Book(1, "Kirja 2", "George Orwell", 1945, "72cb64i7", 15));
+        /*
+         * books.add(new Book(0, "Kirja 1", "Ernest Hemingway", 1929, "vc5y45uv236",
+         * 10));
+         * books.add(new Book(1, "Kirja 2", "George Orwell", 1945, "72cb64i7", 15));
+         */
         model.addAttribute("books", books);
         return "index";
     }
-    
+
 }
 
-
-
-
-
-
-
-/* 5. Bookstore 
-We will now start to create our training project. This excercise continues through the whole course 
-and it will be returned to Github. 
-a.) Create a project called Bookstore by using Spring Initializr page (include web, devtools, 
-thymeleaf) 
-b.) Add a new controller called BookController which handle get request to the path /index 
-c.) Add a new model class called Book which contains attributes: title, author, publicationYear, 
-isbn, price 
-d.) Add your project to your GitHub account (See the instructions from the moodle)
-Follow the recommended Spring Boot project structure */
+/*
+ * 5. Bookstore
+ * We will now start to create our training project. This excercise continues
+ * through the whole course
+ * and it will be returned to Github.
+ * a.) Create a project called Bookstore by using Spring Initializr page
+ * (include web, devtools,
+ * thymeleaf)
+ * b.) Add a new controller called BookController which handle get request to
+ * the path /index
+ * c.) Add a new model class called Book which contains attributes: title,
+ * author, publicationYear,
+ * isbn, price
+ * d.) Add your project to your GitHub account (See the instructions from the
+ * moodle)
+ * Follow the recommended Spring Boot project structure
+ */
