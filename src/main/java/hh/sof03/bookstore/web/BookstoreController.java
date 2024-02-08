69bcf0009e59b19d67d0b816842f15af58c2ea81
amppavai <ani.vainio@myy.haaga-helia.fi>
2024-02-08 13:12:11 +0200

    tehtava 2: booklist

--- src/main/java/hh/sof03/bookstore/web/BookstoreController.java
+++ src/main/java/hh/sof03/bookstore/web/BookstoreController.java
@@ -1,8 +1,6 @@
 package hh.sof03.bookstore.web;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,21 +8,32 @@ import org.springframework.web.bind.annotation.RequestMethod;
 //import org.springframework.web.bind.annotation.RequestParam;
 
 import hh.sof03.bookstore.domain.Book;
+import hh.sof03.bookstore.domain.BookRepository;
 
 @Controller
 public class BookstoreController {
-    // b.) Add a new controller called BookController which handle get request to
-    // the path /index
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    //etu-/aloitussivu
     @RequestMapping(value = "/index", method = RequestMethod.GET)
     public String getBooks(Model model) {
-        List<Book> books = new ArrayList<>();
-        /*
+        /*List<Book> books = new ArrayList<>();
+        
          * books.add(new Book(0, "Kirja 1", "Ernest Hemingway", 1929, "vc5y45uv236",
          * 10));
          * books.add(new Book(1, "Kirja 2", "George Orwell", 1945, "72cb64i7", 15));
          */
-        model.addAttribute("books", books);
-        return "index";
+        model.addAttribute("books", bookRepository.findAll());
+        return "index"; //index.html
+    }
+    //kirjojen listaus
+    @RequestMapping(value = "/booklist", method = RequestMethod.GET)
+    public String listBooks(Model model) {
+
+        model.addAttribute("books", bookRepository.findAll());
+        return "booklist"; //booklist.html
     }
 
 }
